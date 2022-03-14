f32d5c2fc832aca21b70e12f0171ffa31cab9062
Crackhoven <jan-fiedler@hotmail.com>
2022-03-14 10:56:18 +0100

    Exercise 3 done!

--- Jan/test.php
+++ Jan/test.php
@@ -10,8 +10,14 @@
        $name = "Jan Fiedler";
        $age = 24;
        $job_title = "without a job.";
+       $players = array("Mark","John","George","Lisa");
+
        echo"<h1>$name</h1><br>"; 
-       echo "Hi, my name is " . $name . ", I am " . $age . " years old and currently " .$job_title; ?>
+       
+       echo "Hi, my name is " . $name . ", I am " . $age . " years old and currently " .$job_title . "<br>";
+       
+       echo "The third player in the team is " .$players[2] . "<br>";
+       ?>
 
     
    </body>
