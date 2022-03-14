d2955f4c096674fef348ba153468dad101be75f1
peru3232 <prw@qmail.com.de>
2022-03-14 11:27:55 +0100

    Adv update

--- Peter/Advanced.php
+++ Peter/Advanced.php
@@ -1,1 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Day 1 Advanced</title>
+    <link href="styles/styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
 <?php
+    //Step 1:
+    $obj_characters = ["cartoon" => "Donald Duck", "anime" => "Om nom", "game" => "Nameless"];
+    echo "A cartoon hero was: ".$obj_characters['cartoon'];
+    echo "<br>The name of a game hero was: ".$obj_characters["game"];
+
+    //Step 2:
+    
+?>
+</body>
+</html>
