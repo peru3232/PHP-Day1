c0b08fbf9c2772a1715adc7094d8aa50eca72778
peru3232 <prw@qmail.com.de>
2022-03-14 09:49:06 +0100

    start

---
+++ Peter/testn.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Day 1</title>
+</head>
+<body>
+<?php
+$name = "Peter Rudolf";
+$lastName = "Wochinger";
+echo "Hello $name $lastName,<br/> it´s ";
+echo date("D d M Y H:i:s");
+echo "<br/> yeah";
+phpinfo();
+
+?>
+</body>
+</html>
