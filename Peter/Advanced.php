5488a8cc67cb475bdb1638fc3c73c51ad083e7cd
peru3232 <prw@qmail.com.de>
2022-03-14 11:48:35 +0100

    adv done

--- Peter/Advanced.php
+++ Peter/Advanced.php
@@ -8,12 +8,28 @@
 <body>
 <?php
     //Step 1:
-    $obj_characters = ["cartoon" => "Donald Duck", "anime" => "Om nom", "game" => "Nameless"];
-    echo "A cartoon hero was: ".$obj_characters['cartoon'];
-    echo "<br>The name of a game hero was: ".$obj_characters["game"];
+    $aa_characters = ["cartoon" => "Donald Duck", "anime" => "Om nom", "game" => "Nameless"];
+    echo "A cartoon hero was: ".$aa_characters['cartoon'];
+    echo "<br>The name of a game hero was: ".$aa_characters["game"]."<br>";
 
-    //Step 2:
-    
+    //Step 2 (Expand to multidimensional):
+    $obj_characters = array(
+        "cartoon" => array(
+            "name" => "Donald Duck",
+            "kind" => "duck"
+        ),
+        "anime" => array(
+            "name" => "Om nom",
+            "kind" => "phantasy"
+        ),
+        "game" => array(
+            "name" => "Nameless",
+            "kind" => "human"
+        )
+        );
+
+    echo "<br><h3>Discription of the choosen character: His name was ".$obj_characters["cartoon"]["name"]." and he is a ".$obj_characters["cartoon"]["kind"].".</h3>";
+
 ?>
 </body>
 </html>
